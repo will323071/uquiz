5f4c064f68d569b81855c23b708ff9e248c3d5e6
cylee323071@gmail.com <cylee323071@gmail.com>
2025-11-28 16:27:35 +0900

    UQuiz

--- uquiz/src/App.jsx
+++ uquiz/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Quiz from "./pages/Quiz";
 import Results from "./pages/Results";
@@ -6,14 +6,12 @@ import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <BrowserRouter>
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="quiz/:nickname" element={<Quiz />} />
       <Route path="results/:nickname" element={<Results />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
-    </BrowserRouter>
   );
 }
 
